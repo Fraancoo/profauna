8eaea831722910e8d4dd23e99f71bbf90bfdbb4c
FrancoProfauna <fernandofranco_sistemas@profauna.com.mx>
2023-08-30 17:01:03 -0700

    Backup 8/30/23

--- js/home.js
+++
@@ -1,8 +0,0 @@
-const downArrow = document.getElementById("down_arrow");
-const divUs = document.getElementById("div_us");
-
-divUs.tabIndex = "-1";
-
-downArrow.addEventListener("click", () => {
-  divUs.focus();
-});
