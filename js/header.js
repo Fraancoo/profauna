ab343cbe4999f3b102501f7f817bb7521d75f4d2
FrancoProfauna <fernandofranco_sistemas@profauna.com.mx>
2023-09-07 17:01:28 -0700

    Backup 9/7/23

--- js/header.js
+++ js/header.js
@@ -1,4 +1,6 @@
 const header = document.getElementById("header");
+const hamburger = document.getElementById("hamburger");
+const navPortrait = document.getElementById("nav_portrait");
 
 window.onwheel = async (e) => {
   if (e.wheelDeltaY > 0) {
@@ -9,3 +11,17 @@ window.onwheel = async (e) => {
     showNav = false;
   }
 };
+
+function handleHam(status) {
+  const [back, navList] = navPortrait.children;
+  const [ham1, ham2, ham3] = hamburger.children;
+
+  ham1.style = status ? "transform: translateY(12px) rotate(45deg);" : "";
+  ham2.style = status ? "opacity: 0;" : "";
+  ham3.style = status ? "transform: translateY(-12px) rotate(-45deg);" : "";
+  back.style = status ? "opacity: 1;" : "opacity: 0;";
+
+  // if (status) {
+  //   navList
+  // }
+}
