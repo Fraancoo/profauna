2ec0980d278aaa0462343607ecd95d7eb13ea083
FrancoProfauna <fernandofranco_sistemas@profauna.com.mx>
2023-09-13 17:02:08 -0700

    Backup 9/13/23

--- js/servicios.js
+++ js/servicios.js
@@ -1,6 +1,10 @@
 const opts = document.getElementById("opts"),
   data = document.getElementById("data");
 
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+slide(true);
+
 function selectOpt(optId, dataId) {
   for (const o of opts.children) {
     if (optId === o.id) o.className = "active";
@@ -10,9 +14,23 @@ function selectOpt(optId, dataId) {
   for (const d of data.children) {
     const [opt, cont] = d.children;
     if (dataId === d.id) {
-      cont.className = "shown";
+      opt.classList.add("active");
+      cont.classList.replace("hiden", "shown");
     } else {
-      cont.className = "hiden";
+      opt.classList.remove("active");
+      cont.classList.replace("shown", "hiden");
     }
   }
+}
+
+async function slide(dir) {
+  const slider = document.getElementById("slider");
+
+  slider.style.transform = `translateX(-${slider.clientWidth * Number(dir)}px)`;
+
+  // if (dir) slider.style.transform = `translateX(-${slider.clientWidth * 1}px)`;
+  // else slider.style.transform = `translateX(-${slider.clientWidth * 0}px)`;
+
+  await delay(5000);
+  slide(!dir);
 }
